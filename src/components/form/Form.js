1ecbe0d38a47d6d44d0a0ecc0fe2ad389b49ba0d
Elijahscriptdev <elijahobominuru@gmail.com>
2021-02-20 15:47:33 +0100

    fixed warnings

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,16 +1,16 @@
 import React from "react";
-import {
-  fade,
-  ThemeProvider,
-  withStyles,
-  makeStyles,
-  createMuiTheme,
-} from "@material-ui/core/styles";
-import InputBase from "@material-ui/core/InputBase";
+// import {
+//   fade,
+//   ThemeProvider,
+//   withStyles,
+//   makeStyles,
+//   createMuiTheme,
+// } from "@material-ui/core/styles";
+// import InputBase from "@material-ui/core/InputBase";
 import InputLabel from "@material-ui/core/InputLabel";
-import TextField from "@material-ui/core/TextField";
+// import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
-import { green } from "@material-ui/core/colors";
+// import { green } from "@material-ui/core/colors";
 
 const Form = () => {
   return (

b9a5c94de2a8c04ba4e86487f89bfcf25c387d98
Elijahscriptdev <elijahobominuru@gmail.com>
2021-02-20 01:18:28 +0100

    styled navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Lorem ipsum dolor sit amet.</h1>
+      <NavBar />
     </div>
   );
 }

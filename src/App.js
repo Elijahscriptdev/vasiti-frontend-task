a0e57668098894b018b1ce97169cfaccc7c18b05
Elijahscriptdev <elijahobominuru@gmail.com>
2021-02-20 14:48:39 +0100

    add build file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
 import NavBar from "./components/navbar/NavBar";
 import Hero from "./components/hero/Hero";
 import Testimony from "./components/testimonies/Testimony";
 import Footer from "./components/footer/Footer";
+import "./App.css";
 
 function App() {
   return (
